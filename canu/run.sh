7ff86c0391eefd00f243c621fff32bddfdf1b021
GJYWang <gjywang@gmail.com>
2025-02-07 13:51:37 +0100

    Create run.sh

---
+++ canu/run.sh
@@ -0,0 +1,2 @@
+canu -p asm -d $Assem_Name genomeSize=10M -nanopore $Selected_FQ executiveMemory=400G executiveThreads=60
+canu -p asm -d $Assem_Name genomeSize=20k -nanopore $Selected_FQ executiveMemory=400G executiveThreads=60
